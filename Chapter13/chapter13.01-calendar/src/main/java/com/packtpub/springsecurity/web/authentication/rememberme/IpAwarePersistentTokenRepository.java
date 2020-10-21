420b31955685515c095fc65f81d8253788ff3d77
yyi <yyi.mailer@gmail.com>
2020-10-21 14:01:35 +0800

    update remember me

---
+++ Chapter13/chapter13.01-calendar/src/main/java/com/packtpub/springsecurity/web/authentication/rememberme/IpAwarePersistentTokenRepository.java
@@ -0,0 +1,9 @@
+package com.packtpub.springsecurity.web.authentication.rememberme;
+
+public class IpAwarePersistentTokenRepository {
+
+    public IpAwarePersistentTokenRepository(JpaPersistentTokenRepository jpaPersistentTokenRepository) {
+
+    }
+
+}
